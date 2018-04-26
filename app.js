354aac13a92b0f17e6f9db8c535281aa5535c63c
William Strauss <wstrauss123@gmail.com>
2018-04-26 16:17:18 -0400

    pushing nasa gif

---
+++ app.js
@@ -0,0 +1,22 @@
+function displayNasaImage(xhr) {
+    response = xhr.response;
+    let nasaObject = JSON.parse(response);
+    $("#space").append("<img src='" + nasaObject.hdurl + "'></img></a>");
+    $("#space").append("<div class='dynostyle size'>" + nasaObject.title + "</div>");
+    $("#space").append("<div class='dynostyle'>" + nasaObject.explanation + "</div>");
+}
+
+let baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=IUABdnUJEP0Fmr2KwpxWQShXfr8NVfV1NRzOYkMN';
+
+function sendAjaxGet(url, func) {
+    let xhr = new XMLHttpRequest() || new ActiveXObject("Microsoft.HTTPRequest");
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            func(this);
+        }
+    }
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+sendAjaxGet(baseUrl, displayNasaImage);
